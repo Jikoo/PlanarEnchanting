20fcbb5762d4b8fcc3422fac1f29eb92b5f5686f
Jikoo <jikoo.games@gmail.com>
2026-01-20 11:56:05 -0500

    Add mocks for Paper tags to registries

--- src/test/java/com/github/jikoo/planarenchanting/util/mock/server/TestRegistryAccess.java
+++ src/test/java/com/github/jikoo/planarenchanting/util/mock/server/TestRegistryAccess.java
@@ -1,24 +1,21 @@
 package com.github.jikoo.planarenchanting.util.mock.server;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
-import com.github.jikoo.planarenchanting.util.mock.inventory.ItemStackMocks;
 import io.papermc.paper.registry.RegistryAccess;
 import io.papermc.paper.registry.RegistryKey;
+import io.papermc.paper.registry.tag.Tag;
+import io.papermc.paper.registry.tag.TagKey;
+import java.lang.reflect.Field;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BiConsumer;
 import org.bukkit.Keyed;
-import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Registry;
-import org.bukkit.inventory.ItemType;
 import org.jetbrains.annotations.NotNull;
 import org.jspecify.annotations.Nullable;
 import org.mockito.stubbing.Answer;
@@ -39,36 +36,19 @@ public class TestRegistryAccess implements RegistryAccess {
     return (Registry<T>) REGISTERS.computeIfAbsent(registryKey, regKey -> {
       Registry<?> registry = mock();
 
-      AtomicReference<Class<? extends Keyed>> clazzRef = new AtomicReference<>();
-      RegistryHelper.forEachRegistryClass((field, clazz) -> {
+      AtomicReference<Class<T>> clazzRef = new AtomicReference<>();
+      RegistryHelper.forEachRegistryClass((BiConsumer<Field, Class<T>>) (field, clazz) -> {
         if (RegistryHelper.getRegistryKey(clazz).equals(regKey)) {
           clazzRef.set(clazz);
         }
       });
-      Map<NamespacedKey, Keyed> cache = new HashMap<>();
-      Class<? extends Keyed> clazz = clazzRef.get();
 
-      Answer<Keyed> getOrThrow = invocationGetEntry -> {
+      Map<NamespacedKey, T> values = new HashMap<>();
+      Class<T> clazz = clazzRef.get();
+
+      Answer<T> getOrThrow = invocationGetEntry -> {
         NamespacedKey key = invocationGetEntry.getArgument(0);
-        // Some classes (like BlockType and ItemType) have extra generics that will be
-        // erased during runtime calls. To ensure accurate typing, grab the constant's field.
-        // This approach also allows us to return null for unsupported keys.
-        Class<? extends Keyed> constantClazz;
-        try {
-          constantClazz = (Class<? extends Keyed>) clazz.getField(key.getKey().toUpperCase(
-              Locale.ROOT).replace('.', '_')).getType();
-        } catch (ClassCastException | NoSuchFieldException e) {
-          throw new RuntimeException(e);
-        }
-
-        return cache.computeIfAbsent(key, key1 -> {
-          Keyed keyed = mock(constantClazz);
-          doReturn(key).when(keyed).getKey();
-          if (keyed instanceof ItemType itemType) {
-            setUp(itemType);
-          }
-          return keyed;
-        });
+        return values.computeIfAbsent(key, key1 -> RegistryHelper.getOrThrow(clazz, key1));
       };
 
       doAnswer(getOrThrow).when(registry).getOrThrow(any(NamespacedKey.class));
@@ -84,31 +64,15 @@ public class TestRegistryAccess implements RegistryAccess {
         }
       }).when(registry).get(any(NamespacedKey.class));
 
+      Map<TagKey<T>, Tag<T>> tags = new HashMap<>();
+
+      doAnswer(invocation -> {
+        TagKey<T> tagKey = invocation.getArgument(0);
+        return tags.computeIfAbsent(tagKey, key -> RegistryHelper.getTag(clazz, key));
+      }).when(registry).getTag(any(TagKey.class));
+
       return registry;
     });
-  }
-
-  private static void setUp(@NotNull ItemType itemType) {
-    // ItemStack creation.
-    doAnswer(
-        invocation -> {
-          itemType.createItemStack(1);
-          return null;
-        }
-    ).when(itemType).createItemStack();
-    doAnswer(invocation -> ItemStackMocks.newItemMock(itemType, invocation.getArgument(0)))
-        .when(itemType).createItemStack(anyInt());
-
-    // Lazy ItemType -> Material mapping for use in ItemStack creation.
-    Map<ItemType, Material> asMaterial = new ConcurrentHashMap<>();
-    doAnswer(invocation -> asMaterial.computeIfAbsent(itemType, type -> {
-      for (Material material : Material.values()) {
-        if (!material.isLegacy() && material.getKey().equals(itemType.getKey())) {
-          return material;
-        }
-      }
-      throw new IllegalStateException("Unable to locate Material for ItemType " + itemType.getKey());
-    })).when(itemType).asMaterial();
   }
 
 }
